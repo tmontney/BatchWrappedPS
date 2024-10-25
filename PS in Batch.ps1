9c836c77e0155569d2d15627cfcd529ef9c3dd2c
Tyler Montney <tyler.montney@aircar.com>
2024-10-25 09:45:46 -0500

    * Added support to use PowerShell 7. * Added more control over how underlying PowerShell instance is called. * Improved parameter names and types. * Added warning if command length exceeds cmd.exe line limit.

--- PS in Batch.ps1
+++ PS in Batch.ps1
@@ -2,33 +2,56 @@ function ConvertTo-PSBatchScript {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [String]
-        $PSScriptContents,
+        [ScriptBlock]
+        $Action,
         [Parameter(Mandatory = $false)]
         [String[]]
-        $Arguments = @(),
+        $ArgumentList = @(),
         # Consider using delayed expansion on variables
         # If you explicitly specify $env:TEMP and run this as another user, it will map to the wrong user's temp folder
         [Parameter(Mandatory = $false)]
         [String]
-        $CurrentWorkingDirectory = "%TEMP%"
+        $CurrentWorkingDirectory = "%TEMP%", # Ignored if 'AsSingleLine' specified
+        [Parameter(Mandatory = $false)]
+        [Switch]
+        $UsePowerShell7,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("AllSigned", "Bypass", "Default", "RemoteSigned", "Restricted", "Undefined", "Unrestricted")]
+        [String]
+        $ExecutionPolicy = "Default",
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Normal", "Minimized", "Maximized", "Hidden")]
+        [String]
+        $WindowStyle = "Normal",
+        [Parameter(Mandatory = $false)]
+        [Switch]
+        $ScheduledTask # Returns as an array instead: {powershell path} {powershell arguments} {base64 scriptblock} {base64 scriptblock arguments}
     )
-    
-    try 
-    { [void]([ScriptBlock]::Create($PSScriptContents)) }
-    catch 
-    { Write-Warning -Message "There was a problem parsing 'PSScriptContents' as a PowerShell script."; Write-Error $_; return }
 
-    if ($Arguments.Count -gt 0) {
+    # If PowerShell goes to 8, this will probably break.
+    $FilePath = "$env:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe"
+    if ($UsePowerShell7)
+    { $FilePath = "$env:PROGRAMFILES\PowerShell\7\pwsh.exe" }
+
+    if (-not (Test-Path -Path $FilePath))
+    { Write-Warning -Message "'$FilePath' does not exist"; return }
+
+    if ($ArgumentList.Count -gt 0) {
         $ArgumentsAL = [System.Collections.ArrayList]::new()
-        $ArgumentsAL.AddRange($Arguments)
+        $ArgumentsAL.AddRange($ArgumentList)
 
         $cliXml = [System.Management.Automation.PSSerializer]::Serialize($ArgumentsAL)
         $ArgsBase64 = "-EncodedArguments " + ([Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($cliXml)))
     }
 
-    $BatchBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($PSScriptContents))
-    $InvokePSLine = "%WINDIR%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -EncodedCommand $BatchBase64 $ArgsBase64"
+    $BatchBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($Action.ToString()))
+    $InvokePSLine = "$FilePath -ExecutionPolicy Bypass -EncodedCommand $BatchBase64 $ArgsBase64"
+
+    if ($InvokePSLine.Length -gt 32767)
+    { Write-Warning -Message "The generated command is greater than 32,767 characters; cmd.exe will fail to run this." }
+
+    if ($ScheduledTask)
+    { return @($FilePath, "-ExecutionPolicy $ExecutionPolicy -WindowStyle $WindowStyle", "-EncodedCommand $BatchBase64", $ArgsBase64) }
 
     return @"
 @echo off
@@ -54,4 +77,4 @@ else {
 }
 
 $PSScriptContents = Get-Content -Path "$Script:ScriptCWD\Sample PS Script.ps1" -Raw
-ConvertTo-PSBatchScript -PSScriptContents $PSScriptContents -Arguments @("Hello world!") | Set-Content -LiteralPath "$Script:ScriptCWD\Sample PS Script.bat" -Force
+ConvertTo-PSBatchScript -Action ([ScriptBlock]::Create($PSScriptContents)) -ArgumentList @("Hello world!") -ExecutionPolicy Bypass | Set-Content -LiteralPath "$Script:ScriptCWD\Sample PS Script.bat" -Force
